69b832853174cb2c7412bf8604ae7f31913d95cf
Mac <mac@Admins-MacBook-Pro.local>
2026-04-11 06:55:06 +0100

    fix render port

--- run_api.py
+++ run_api.py
@@ -1,12 +1,9 @@
 import os
-
 import uvicorn
-
 
 if __name__ == '__main__':
     uvicorn.run(
         'backend.main:app',
-        host=os.getenv('FASTAPI_HOST', '127.0.0.1'),
-        port=int(os.getenv('FASTAPI_PORT', '8000')),
-        reload=os.getenv('FASTAPI_RELOAD', '0') == '1',
+        host='0.0.0.0',
+        port=int(os.environ.get('PORT', 8000)),
     )
